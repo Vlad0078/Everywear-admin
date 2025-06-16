a836ddaa8b76488a1b0226915d5e719fcdb8712f
Vlad Buhrym <rymlian.continuum@gmail.com>
2025-06-16 10:39:33 +0300

    Load1

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-	server: {
-    host: '0.0.0.0', // Дозволяє приймати зовнішні з’єднання
-    port: 10000, // Render часто використовує порт 10000 для Web Services
-    strictPort: true, // Не дозволяє Vite змінювати порт, якщо він зайнятий
+  server: {
+    host: "192.168.0.108",
+    port: 5175,
   },
 });
